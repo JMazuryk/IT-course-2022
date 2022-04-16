59531a0106e7aeab988b1324d9103f7542b15c9c
JMazuryk <Julia.Mazuryk@gmail.com>
2022-04-16 17:34:22 +0300

    Lesson 5

---
+++ JS - 2022/Lesson 5/homework/main.js
@@ -0,0 +1,172 @@
+// let a1 = 'hello';
+// let a2 = 'owu';
+// let a3 = 'com';
+// let a4 = 'ua';
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3;14;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+//
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+
+//
+// let a1 = 'hello';
+// let a2 = 'owu';
+// let a3 = 'com';
+// let a4 = 'ua';
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3;14;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+//
+// alert(a1);
+// alert(a2);
+// alert(a3);
+// alert(a4);
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// alert(a11);
+// alert(a12);
+// alert(a13);
+
+//
+// let a1 = 'hello';
+// let a2 = 'owu';
+// let a3 = 'com';
+// let a4 = 'ua';
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3.14;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+// document.write(a1);
+// document.write(a2);
+// document.write(a3);
+// document.write(a4);
+// document.write(a5);
+// document.write(a6);
+// document.write(a7);
+// document.write(a8);
+// document.write(a9);
+// document.write(a10);
+// document.write(a11);
+// document.write(a12);
+// document.write(a13);
+
+
+//
+// let book1 = {
+//     name: 'Chanel',
+//     pages: 450,
+//     genre: 'biography'
+// }
+//
+// console.log(book);
+
+// let book2 = {
+//     name: 'Chanel',
+//     pages: 450,
+//     genre: 'biography',
+//     authors: [
+//         'Coco Chanel',
+//         'Anri Hidel',
+//         'John Roberts'
+//     ]
+// }
+// console.log(book2);
+
+
+// let firstName = 'Julia';
+// let middleName = 'Romanivna';
+// let lastName = 'Mazuryk';
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+// console.log (person);
+//
+//let firstName = 'Julia';
+//let middleName = 'Romanivna';
+//let lastName = 'Mazuryk';
+//let person = `${firstName} ${middleName} ${lastName}`;
+//console.log(person);
+
+
+// let firstName = prompt( 'enter your first name') ;
+// let middleName = prompt( 'enter your middle name') ;
+// let lastName = prompt( 'enter your last name') ;
+// let person = `${firstName} ${middleName} ${lastName}`
+// console.log(person)
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+// let books = [
+//     {
+//         name: 'Chanel',
+//         pages: 300,
+//         genre: 'biography',
+//         authors: ['Coco Chanel',
+//             'Anri Hidel',
+//             'John Roberts'],
+//     },
+//
+//     {
+//         name: 'Three Comrades',
+//         pages: 450,
+//         genre: 'novel',
+//         author: 'Erich Maria Remarque'
+//
+//     },
+//
+//     {
+//         name: 'Hercule Poirot',
+//         pages: 550,
+//         genre: 'detective',
+//         author: 'Agatha Christie'
+//     }
+// ]
+//
+// console.log(books[0]);
+// console.log(books[1]);
+// console.log(books[2]);
+
+
+
+
+
+
+
